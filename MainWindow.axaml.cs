14cb5d49e2ee7fb10ec5d260704b86011643f8a3
combstraight <yoonsoh@protonmail.com>
2024-11-01 17:14:36 +0700

    Initial commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace blood_glucose_tracker;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
